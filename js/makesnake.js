2931ea29a88cfcef7af0f09cd70835e7755030ab
wangyongli <17302010031@fudan.edu.cn>
2022-02-06 22:34:22 +0800

    modify content

--- js/makesnake.js
+++ js/makesnake.js
@@ -1,6 +1,6 @@
 ﻿var x,y;		//鼠标当前在页面上的位置
-var step=10;	//字符显示间距，为了好看，step=0则字符显示没有间距
-var message="51bbw.cn";		//跟随鼠标要显示的字符串
+var step=20;	//字符显示间距，为了好看，step=0则字符显示没有间距
+var message="姐姐❤弟弟";		//跟随鼠标要显示的字符串
 message=message.split("");	//将字符串分割为字符数组
 
 var xpos=new Array()		//存储每个字符的x位置的数组
@@ -33,14 +33,14 @@ function makesnake() {  //重定位每个字符的位置
 	if (document.all) { //如果是IE
 		for (i=message.length-1; i>=1; i--) {
 			xpos[i]=xpos[i-1]+step;  //从尾向头确定字符的位置，每个字符为前一个字符“历史”水平坐标+step间隔，
-            //这样随着光标移动事件，就能得到一个动态的波浪状的显示效果
+			//这样随着光标移动事件，就能得到一个动态的波浪状的显示效果
 			ypos[i]=ypos[i-1];  //垂直坐标为前一字符的历史“垂直”坐标，后一个字符跟踪前一个字符运动
 		}
 		xpos[0]=x+step //第一个字符的坐标位置紧跟鼠标光标
 		ypos[0]=y
 		//上面的算法将保证，如果鼠标光标移动到新位置，则连续调用makenake将会使这些字符一个接一个的移动的新位置
-		// 该算法显示字符串就有点象人类的游行队伍一样， 
-	
+		// 该算法显示字符串就有点象人类的游行队伍一样，
+
 		for (i=0; i<=message.length-1; i++) {
 			var thisspan = eval("span"+(i)+".style");  //妙用eval根据字符串得到该字符串表示的对象
 			thisspan.posLeft=xpos[i];
